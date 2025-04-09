b2892e8c4467a6807beb678547ffa32771670076
panyi <451360111@qq.com>
2025-04-09 17:35:54 +0800

    修改webpack配置来处理axios模块路径

--- histomicsui/web_client/webpack.helper.js
+++ histomicsui/web_client/webpack.helper.js
@@ -35,11 +35,23 @@ module.exports = function (config) {
             require.resolve('vue-loader')
         ]
     });
+    // config.resolve = {
+    //     alias: {
+    //         vue: process.env.NODE_ENV === 'production' ? 'vue/dist/vue.min.js' : 'vue/dist/vue.js'
+    //     }
+    // };
     config.resolve = {
         alias: {
-            vue: process.env.NODE_ENV === 'production' ? 'vue/dist/vue.min.js' : 'vue/dist/vue.js'
-        }
+            vue: process.env.NODE_ENV === 'production' ? 'vue/dist/vue.min.js' : 'vue/dist/vue.js',
+            'axios$': path.resolve(__dirname, 'node_modules/axios/dist/axios.min.js')
+        },
+        modules: [
+            path.resolve(__dirname, 'node_modules'),
+            'node_modules'
+        ]
     };
+    
+    config.module.noParse = /axios\.min\.js$/;
     config.plugins.push(new VueLoaderPlugin());
     return config;
 };
